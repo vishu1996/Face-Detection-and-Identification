58fe2122a748be6f0160e27c18b6ca5e6c348f9f
vishu1996 <vishu7by1996@gmail.com>
2019-03-07 22:04:29 +0530

    Update face_dataset_collection_live.py

--- face_dataset_collection_live.py
+++ face_dataset_collection_live.py
@@ -2,27 +2,27 @@ import numpy as np
 
 import cv2
 
-face_cascade = cv2.CascadeClassifier('C:/Users/User/Desktop/New folder (2)/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml')
+haar_cascade_face = cv2.CascadeClassifier('C:/Your_Path_of_Opencv_file/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 
 id = input('Enter user id : ')
 
-sampleN=0;
+photos=0;
 
 while 1:
 
-    ret, img = cap.read()
+    ret, img = camera.read()
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = haar_cascade_face.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
 
-        sampleN=sampleN+1;
+        photos=photos+1;
 
-        cv2.imwrite("C:/Users/User/Desktop/New folder (2)/face_dataset/User."+str(id)+ "." +str(sampleN)+ ".jpg", gray[y:y+h, x:x+w])
+        cv2.imwrite("C:/Users/User/Desktop/New folder (2)/face_dataset/User."+str(id)+ "." +str(photos)+ ".jpg", gray[y:y+h, x:x+w])
 
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
@@ -32,10 +32,10 @@ while 1:
 
     cv2.waitKey(1)
 
-    if sampleN >= 10:
+    if photos >= 10:        #i.e. if no. of photos clicked by computer is more than 10, then terminate clicking photos
 
         break
 
-cap.release()
+camera.release()
 
 cv2.destroyAllWindows()
